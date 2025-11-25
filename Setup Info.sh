fecfe931d9144e3b4019aaa86aff5a6850667bd5
Daniel Morvay <morvayd@gmail.com>
2025-11-24 19:10:18 -0500

    Create Setup Info.sh with installation instructions
    Added setup instructions for installing dependencies and running the app.

---
+++ Setup Info.sh
@@ -0,0 +1,17 @@
+#  Author:  Daniel Morvay
+#  Creator Email:  morvayd@gmail.com
+
+#
+#  ---------- CLI Install ----------
+#
+
+#  From the system Command Line Interface (CLI)
+pip install pandas
+
+
+#  Note:  
+#  PythonLog.py - keep in the same folder as Python-Library-Locate.py
+#  This will create a PythonLogs folder and save app logs there.  
+
+#  Run the app
+python3 Python-Library-Locate.py
