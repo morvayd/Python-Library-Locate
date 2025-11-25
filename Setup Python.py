d896004446f6d16ea5e82bb823e8c82040d92665
Daniel Morvay <morvayd@gmail.com>
2025-11-24 19:10:48 -0500

    Create Setup Python.py for library installation
    Added initial setup script for installing additional libraries.

---
+++ Setup Python.py
@@ -0,0 +1,11 @@
+
+#  Author:  Daniel Morvay
+#  Creator Email:  morvayd@gmail.com
+
+#
+#  ---------- Install Additional Libraries ---------- 
+#
+
+#  install the additional libraries before running.
+#  Edit strPackList and enter package names you would like to search for.  
+
